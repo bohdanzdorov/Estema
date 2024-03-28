1f35f1cb58cbb3d2713c1cc9a2058b4ac7911538
Bohdan Zdorovets <zdorovetsbogdan@gmail.com>
2024-03-28 23:46:40 +0100

    WordsListItem added

--- frontend/src/Pages/MainPage.js
+++ frontend/src/Pages/MainPage.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Navbar from '../Components/Navbar';
+import Stack from '@mui/material/Stack';
+import WordsListItem from '../Components/WordsListItem';
+
 
 function MainPage() {
   return (
     <div className="MainPage">
       <Navbar/>
-
+      <Stack>
+        <WordsListItem name={"newList"} fromLanguage={"ENG"} toLanguage={"UKR"}/>
+      </Stack>
     </div>
   );
 }
