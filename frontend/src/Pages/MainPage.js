8984bd1683fea01907d3777416b32348f950744d
Bohdan Zdorovets <zdorovetsbogdan@gmail.com>
2024-03-28 23:11:13 +0100

    created new react project for front-end. Added navbar

---
+++ frontend/src/Pages/MainPage.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Navbar from '../Components/Navbar';
+
+function MainPage() {
+  return (
+    <div className="MainPage">
+      <Navbar/>
+
+    </div>
+  );
+}
+
+export default MainPage;
